cfcb1536da1f3401d3c2bc6b17e09e3a0c379318
Sean Spiegl <sean.spiegl@outlook.com>
2021-10-25 19:29:37 +0800

    added bin name specifier to input args

--- bfcomp.py
+++ bfcomp.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser.add_argument('bfname')
     parser.add_argument('-l', help='Language to compile to', dest='language', default='c', choices=['c', 'asm'])
     parser.add_argument('-o', help='Name of compiled file', dest='cname', default=None)
-    parser.add_argument('-c', help='Whether to use external compiler to compile to binary file', dest='compile', action='store_true')
+    parser.add_argument('-c', help='Whether to use external compiler to compile to binary file', dest='binname', nargs='?', default=False)
 
     args = parser.parse_args()
 
@@ -21,10 +21,15 @@ if __name__ == '__main__':
         if rawname.endswith('.bf'):
             rawname = rawname[:-3]
         cname = rawname + '.' + args.language
+
+    binname = args.binname
+    compile = binname != False
+    if binname == None:
+        binname = 'a.out'
     
     if args.language == 'c':
-        executor = CCompiler(instructions, cname, args.compile, 'a.out')
+        executor = CCompiler(instructions, cname, compile, binname)
     else:
-        executor = AsmCompiler(instructions, cname, args.compile, 'a.out') 
+        executor = AsmCompiler(instructions, cname, compile, binname) 
  
     processor.process(instructions, executor)
