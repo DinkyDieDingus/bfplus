72d65d8548e906128cd3bfc2c29f233aec7f9896
Sean Spiegl <sean.spiegl@outlook.com>
2021-10-28 13:27:18 +0800

    added  looping support to bin compilation

--- src/compiler/bin.py
+++ src/compiler/bin.py
@@ -4,10 +4,10 @@ from src.executor import Executor
 class BinCompiler(Executor):
     def __init__(self, instructions, binname):
         super().__init__(instructions)
-        self.stack = []
         self.binname = binname
 
-        self.text = b'\xc6\x04\x24\x00'
+        self.loopStarts = []
+        self.text = bytearray(b'\xc6\x04\x24\x00')
 
     def finish(self):
         self.text += b'\xb8\x3c\x00\x00\x00' # mov    $0x3c,%eax
@@ -20,6 +20,7 @@ class BinCompiler(Executor):
         file.write(self.text)
         
         file.close()
+        os.system('chmod +x ' + self.binname)
 
     def incPtr(self, i):
         self.text += b'\x48\xff\xcc'
@@ -54,21 +55,49 @@ class BinCompiler(Executor):
         return i
 
     def loopStart(self, i):
-        self.stack.append(len(self.text))
-        """
-        self.file.write(self.pad + 'cmp byte [rsp], 0x0\n')
-        self.file.write(self.pad + f'je {nxt}_end\n')
-        self.file.write(nxt + ':\n')
-        """
+        
+        # check ptr is 0
+        self.text += b'\x80\x3c\x24\x00'
+        # jump to end of loop if so
+        self.text += b'\x74\x00' # leave 0x00 at end to fill in later
+        # bookmark beginning
+        self.loopStarts.append(len(self.text))
+
         return i
 
     def loopEnd(self, i):
-        prev = self.stack[-1]
-        """self.file.write(self.pad + 'cmp BYTE [rsp], 0x0\n')
-        self.file.write(self.pad + f'jne {prev}\n')
-        self.file.write(prev + '_end:\n')"""
+        
+        # check ptr is 0
+        self.text += b'\x80\x3c\x24\x00'
+
+        # get the beginning of the loop
+        start = self.loopStarts.pop()
+        print(f'start = {hex(start)}')
+        # get the number of instructions previous it was
+        start_rel_pos = len(self.text) - start + 2
+        print(f'diff  = {start_rel_pos}')
+        print(f'dihex = {hex(start_rel_pos)}')
+        # convert that number into negative signed 8-bit hex
+        start_rel_pos_hex = twos_compliment(start_rel_pos)
+        print(f'dineg = {hex(start_rel_pos_hex)}')
+        print(f'bytes = {start_rel_pos_hex.to_bytes(1, "big")}')
+        # write jne to that address
+        self.text += b'\x75' + start_rel_pos_hex.to_bytes(1, 'big')
+        
+        end = len(self.text) 
+        print(f'end   = {hex(end)}\n')
+        self.text[start - 1] = end - start
+
         return i
 
+
+def twos_compliment(num):
+    print(bin(num)[2:].rjust(8,'0'))
+    comp = num ^ 0b11111111
+    print(bin(comp)[2:].rjust(8,'0'))
+    res = comp + 1
+    print(bin(res)[2:].rjust(8,'0'))
+    return res
 
 def get_elf_header(text_size):
     # ELF Header
@@ -81,7 +110,7 @@ def get_elf_header(text_size):
     hdr += b'\x02\x00' # exec file type
     hdr += b'\x3e\x00' # x86-64 architecture
     hdr += b'\x01\x00\x00\x00' # ELF ver. 1
-    hdr += get_entry_point() # entry point address
+    hdr += dword(get_entry_point()) # entry point address
     hdr += b'\x40' + b'\x00' * 7 # start of program header table
     hdr += b'\x00'*8 # start of section header (none)
     hdr += b'\x00'*4 # flags (none)
@@ -98,12 +127,15 @@ def get_elf_header(text_size):
     hdr += b'\x00' * 8 # offset
     hdr += (b'\x00'*4 + b'\x00\x40\x00\x00')[::-1] # virtual address
     hdr += (b'\x00'*4 + b'\x00\x40\x00\x00')[::-1] # physical address (fake)
-    hdr += text_size.to_bytes(8, 'little') # size in bytes of file image
-    hdr += text_size.to_bytes(8, 'little') # size in bytes of memory image
+    hdr += dword(text_size) # size in bytes of file image
+    hdr += dword(text_size)# size in bytes of memory image
     hdr += (b'\x00'*4 + b'\x00\x00\x10\x00')[::-1] # alignment
 
     return hdr
 
+def dword(num):
+    return num.to_bytes(8, 'little')
+
 def get_entry_point():
     # vmem address 0x400000 + size of elf header + size of program header
-    return (0x400000 + 0x40 + 0x38).to_bytes(8, 'little')
+    return 0x400000 + 0x40 + 0x38
