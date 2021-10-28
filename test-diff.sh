d1d4e3e8e3cb8a7c34c68a2ad4438a58e126f239
Sean Spiegl <sean.spiegl@outlook.com>
2021-10-28 20:08:44 +0800

    added test script\

--- test-diff.sh
+++ test-diff.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+diff=0
 cut -c11- asm_hex.txt > cut_asm_hex.txt
 cut -c11- bin_hex.txt > cut_bin_hex.txt
-diff cut_asm_hex.txt cut_bin_hex.txt
+diff cut_asm_hex.txt cut_bin_hex.txt >/dev/null
+if [ $? -ne 0 ]; then
+    diff=1
+fi
 rm cut_asm_hex.txt
 rm cut_bin_hex.txt
+exit $diff
